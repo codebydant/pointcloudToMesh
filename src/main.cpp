6beeeab5bf6e6d7c9739d3560aa3a0f727e427e4
Daniel Tobon <danieltobonco@gmail.com>
2022-08-02 15:31:01 -0500

    added cloudparse module

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,17 @@
            HEADERS
 **********************************/
 
-#include <pcl/io/file_io.h>
-#include <pcl/io/io.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/io/ply/ply.h>
-#include <pcl/io/ply/ply_parser.h>
-#include <pcl/io/ply_io.h>
-#include <pcl/io/vtk_io.h>
-#include <pcl/io/vtk_lib_io.h>
-
-#include <pcl/point_types.h>
-
-#include <pcl/visualization/pcl_visualizer.h>
-
-#include <pcl/console/parse.h>
-#include <pcl/console/print.h>
-#include <pcl/console/time.h>
-
-#include <pcl/range_image/range_image.h>
-
+#include <pcl/ModelCoefficients.h>
 #include <pcl/common/common.h>
 #include <pcl/common/common_headers.h>
 #include <pcl/common/geometry.h>
 #include <pcl/common/transforms.h>
-
-#include <pcl/ModelCoefficients.h>
-
+#include <pcl/console/parse.h>
+#include <pcl/console/print.h>
+#include <pcl/console/time.h>
 #include <pcl/features/gasd.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/features/normal_3d_omp.h>
-
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/crop_hull.h>
 #include <pcl/filters/extract_indices.h>
@@ -40,46 +21,42 @@
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/voxel_grid.h>
-
+#include <pcl/io/file_io.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply/ply.h>
+#include <pcl/io/ply/ply_parser.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/vtk_io.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/point_types.h>
+#include <pcl/range_image/range_image.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/search/search.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/segmentation/sac_segmentation.h>
-
 #include <pcl/surface/convex_hull.h>
 #include <pcl/surface/gp3.h>
 #include <pcl/surface/mls.h>
 #include <pcl/surface/poisson.h>
 #include <pcl/surface/simplification_remove_unused_vertices.h>
 #include <pcl/surface/vtk_smoothing/vtk_utils.h>
-
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-
-#include <pcl/search/kdtree.h>
-#include <pcl/search/search.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 #include <boost/algorithm/algorithm.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/thread/thread.hpp>
-
-#include "argparse/argparse.hpp"
 #include <fstream>
 #include <iostream>
 #include <string>
 
-void printUsage(const char *progName) {
-  std::cout << "\nUsage: " << progName
-            << " <input cloud> <surface method> <normal estimation method> "
-               "<output dir>"
-            << std::endl;
-  std::cout << "surface method: \n '1' for poisson \n '2' for gp3" << std::endl;
-  std::cout << "normal estimation method: \n '1' for normal estimation \n '2' "
-               "for mls normal estimation"
-            << std::endl;
-}
+#include "argparse/argparse.hpp"
+#include "cloudparse/parser.hpp"
 
-void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
-                 int &normal_method, pcl::PolygonMesh &triangles) {
-
+void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode, int &normal_method,
+                 pcl::PolygonMesh &triangles) {
   /* ****Translated point cloud to origin**** */
   Eigen::Vector4f centroid;
   pcl::compute3DCentroid(*cloud, centroid);
@@ -87,16 +64,13 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
   Eigen::Affine3f transform = Eigen::Affine3f::Identity();
   transform.translation() << -centroid[0], -centroid[1], -centroid[2];
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudTranslated(
-      new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudTranslated(new pcl::PointCloud<pcl::PointXYZ>());
   pcl::transformPointCloud(*cloud, *cloudTranslated, transform);
 
   /* ****kdtree search and msl object**** */
-  pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree_for_points(
-      new pcl::search::KdTree<pcl::PointXYZ>);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree_for_points(new pcl::search::KdTree<pcl::PointXYZ>);
   kdtree_for_points->setInputCloud(cloudTranslated);
-  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(
-      new pcl::PointCloud<pcl::PointNormal>());
+  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>());
 
   bool mls_mode = false;
   bool normal_mode = false;
@@ -127,11 +101,9 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
   }
 
   if (mls_mode) {
-
     std::cout << "Using mls method estimation...";
 
-    pcl::PointCloud<pcl::PointNormal>::Ptr mls_points(
-        new pcl::PointCloud<pcl::PointNormal>());
+    pcl::PointCloud<pcl::PointNormal>::Ptr mls_points(new pcl::PointCloud<pcl::PointNormal>());
     pcl::MovingLeastSquares<pcl::PointXYZ, pcl::PointNormal> mls;
 
     // Set parameters
@@ -147,11 +119,9 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
     mls.setSearchRadius(0.03);
     mls.process(*mls_points);
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp(
-        new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr temp(new pcl::PointCloud<pcl::PointXYZ>());
 
     for (int i = 0; i < mls_points->points.size(); i++) {
-
       pcl::PointXYZ pt;
       pt.x = cloud->points[i].x;
       pt.y = cloud->points[i].y;
@@ -164,7 +134,6 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
     std::cout << "[OK]" << std::endl;
 
   } else if (normal_mode) {
-
     std::cout << "Using normal method estimation...";
 
     pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> n;
@@ -172,8 +141,8 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
 
     n.setInputCloud(cloudTranslated);
     n.setSearchMethod(kdtree_for_points);
-    n.setKSearch(20);    // It was 20
-    n.compute(*normals); // Normals are estimated using standard method.
+    n.setKSearch(20);     // It was 20
+    n.compute(*normals);  // Normals are estimated using standard method.
 
     // pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new
     // pcl::PointCloud<pcl::PointNormal> ());
@@ -189,14 +158,12 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
   }
 
   // Create search tree*
-  pcl::search::KdTree<pcl::PointNormal>::Ptr kdtree_for_normals(
-      new pcl::search::KdTree<pcl::PointNormal>);
+  pcl::search::KdTree<pcl::PointNormal>::Ptr kdtree_for_normals(new pcl::search::KdTree<pcl::PointNormal>);
   kdtree_for_normals->setInputCloud(cloud_with_normals);
 
   std::cout << "Applying surface meshing...";
 
   if (gp3_mode) {
-
     std::cout << "Using surface method: gp3 ..." << std::endl;
 
     int searchK = 100;
@@ -207,13 +174,13 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
 
     pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
 
-    gp3.setSearchRadius(search_radius);                   // It was 0.025
-    gp3.setMu(setMU);                                     // It was 2.5
-    gp3.setMaximumNearestNeighbors(maxiNearestNeighbors); // It was 100
+    gp3.setSearchRadius(search_radius);                    // It was 0.025
+    gp3.setMu(setMU);                                      // It was 2.5
+    gp3.setMaximumNearestNeighbors(maxiNearestNeighbors);  // It was 100
     // gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees    //it was 4
     // gp3.setMinimumAngle(M_PI/18); // 10 degrees //It was 18
     // gp3.setMaximumAngle(M_PI/1.5); // 120 degrees        //it was 1.5
-    gp3.setNormalConsistency(normalConsistency); // It was false
+    gp3.setNormalConsistency(normalConsistency);  // It was false
     gp3.setInputCloud(cloud_with_normals);
     gp3.setSearchMethod(kdtree_for_normals);
     gp3.reconstruct(triangles);
@@ -228,7 +195,6 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
     std::cout << "[OK]" << std::endl;
 
   } else if (poisson_mode) {
-
     std::cout << "Using surface method: poisson ..." << std::endl;
 
     int nThreads = 8;
@@ -245,17 +211,17 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
 
     pcl::Poisson<pcl::PointNormal> poisson;
 
-    poisson.setDepth(depth); // 9
+    poisson.setDepth(depth);  // 9
     poisson.setInputCloud(cloud_with_normals);
-    poisson.setPointWeight(pointWeight); // 4
+    poisson.setPointWeight(pointWeight);  // 4
     poisson.setDegree(2);
-    poisson.setSamplesPerNode(samplePNode); // 1.5
-    poisson.setScale(scale);                // 1.1
-    poisson.setIsoDivide(isoDivide);        // 8
+    poisson.setSamplesPerNode(samplePNode);  // 1.5
+    poisson.setScale(scale);                 // 1.1
+    poisson.setIsoDivide(isoDivide);         // 8
     poisson.setConfidence(confidence);
     poisson.setOutputPolygons(outputPolygons);
     poisson.setManifold(manifold);
-    poisson.setSolverDivide(solverDivide); // 8
+    poisson.setSolverDivide(solverDivide);  // 8
     poisson.reconstruct(triangles);
 
     // pcl::PolygonMesh mesh2;
@@ -273,11 +239,8 @@ void create_mesh(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, int &surface_mode,
   }
 }
 
-void vizualizeMesh(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud,
-                   pcl::PolygonMesh &mesh) {
-
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
-      new pcl::visualization::PCLVisualizer("MAP3D MESH"));
+void vizualizeMesh(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud, pcl::PolygonMesh &mesh) {
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("MAP3D MESH"));
 
   int PORT1 = 0;
   viewer->createViewPort(0.0, 0.0, 0.5, 1.0, PORT1);
@@ -301,10 +264,8 @@ void vizualizeMesh(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud,
   viewer->addText3D("y", p2, 0.2, 0, 1, 0, "y_");
   viewer->addText3D("z", p3, 0.2, 0, 0, 1, "z_");
 
-  if (cloud->points[0].r <= 0 and cloud->points[0].g <= 0 and
-      cloud->points[0].b <= 0) {
-    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB>
-        color_handler(cloud, 255, 255, 0);
+  if (cloud->points[0].r <= 0 and cloud->points[0].g <= 0 and cloud->points[0].b <= 0) {
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGB> color_handler(cloud, 255, 255, 0);
     viewer->removeAllPointClouds(0);
     viewer->addPointCloud(cloud, color_handler, "original_cloud", PORT1);
   } else {
@@ -338,21 +299,15 @@ std::endl;
 */
 
 int main(int argc, char **argv) {
-
   argparse::ArgumentParser arg_parser(argv[0]);
 
   arg_parser.add_argument("--file").required().help("input cloud file");
-  arg_parser.add_argument("--surface")
-      .default_value(int(2))
-      .scan<'i', int>()
-      .help("surface method 1: poisson 2: gp3");
+  arg_parser.add_argument("--surface").default_value(int(2)).scan<'i', int>().help("surface method 1: poisson 2: gp3");
   arg_parser.add_argument("--normal")
       .default_value(int(1))
       .scan<'i', int>()
       .help("normal estimation method 1:normal 2: mls");
-  arg_parser.add_argument("-o", "--output-dir")
-      .default_value(std::string("-"))
-      .help("output dir to save mesh file");
+  arg_parser.add_argument("-o", "--output-dir").required().help("output dir to save mesh file");
   arg_parser.add_argument("-d", "--display")
       .default_value(false)
       .implicit_value(true)
@@ -366,174 +321,52 @@ int main(int argc, char **argv) {
     std::exit(0);
   }
 
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
-      new pcl::PointCloud<pcl::PointXYZRGB>());
-  pcl::PolygonMesh cl;
-  std::vector<int> filenames;
-  bool file_is_pcd = true;
-  bool file_is_ply = false;
-  bool file_is_txt = false;
-  bool file_is_xyz = false;
-
-  pcl::console::TicToc tt;
-  pcl::console::print_highlight("Loading ");
-
-  std::string filename = arg_parser.get<std::string>("--file");
-
-  if (file_is_pcd) {
-    if (pcl::io::loadPCDFile(filename, *cloud) < 0) {
-      std::cout << "Error loading point cloud " << filename << "\n";
-      return -1;
-    }
-    pcl::console::print_info("\nFound pcd file.\n");
-    pcl::console::print_info("[done, ");
-    pcl::console::print_value("%g", tt.toc());
-    pcl::console::print_info(" ms : ");
-    pcl::console::print_value("%d", cloud->size());
-    pcl::console::print_info(" points]\n");
-  } else if (file_is_ply) {
-    pcl::io::loadPLYFile(filename, *cloud);
-    if (cloud->points.size() <= 0 or cloud->points[0].x <= 0 and
-                                         cloud->points[0].y <= 0 and
-                                         cloud->points[0].z <= 0) {
-      pcl::console::print_warn("\nloadPLYFile could not read the cloud, "
-                               "attempting to loadPolygonFile...\n");
-      pcl::io::loadPolygonFile(filename, cl);
-      pcl::fromPCLPointCloud2(cl.cloud, *cloud);
-      if (cloud->points.size() <= 0 or cloud->points[0].x <= 0 and
-                                           cloud->points[0].y <= 0 and
-                                           cloud->points[0].z <= 0) {
-        pcl::console::print_warn("\nloadPolygonFile could not read the cloud, "
-                                 "attempting to PLYReader...\n");
-        pcl::PLYReader plyRead;
-        plyRead.read(filename, *cloud);
-        if (cloud->points.size() <= 0 or cloud->points[0].x <= 0 and
-                                             cloud->points[0].y <= 0 and
-                                             cloud->points[0].z <= 0) {
-          pcl::console::print_error("\nError. ply file is not compatible.\n");
-          return -1;
-        }
-      }
-    }
-
-    pcl::console::print_info("\nFound ply file.");
-    pcl::console::print_info("[done, ");
-    pcl::console::print_value("%g", tt.toc());
-    pcl::console::print_info(" ms : ");
-    pcl::console::print_value("%d", cloud->size());
-    pcl::console::print_info(" points]\n");
-
-  } else if (file_is_txt) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-      std::cout << "Error: Could not find " << filename << std::endl;
-      return -1;
-    }
-
-    std::cout << "file opened." << std::endl;
-    double x_, y_, z_;
-    unsigned int r, g, b;
-
-    while (file >> x_ >> y_ >> z_ >> r >> g >> b) {
-      pcl::PointXYZRGB pt;
-      pt.x = x_;
-      pt.y = y_;
-      pt.z = z_;
-
-      uint8_t r_, g_, b_;
-      r_ = uint8_t(r);
-      g_ = uint8_t(g);
-      b_ = uint8_t(b);
-
-      uint32_t rgb_ = ((uint32_t)r_ << 16 | (uint32_t)g_ << 8 | (uint32_t)b_);
-      pt.rgb = *reinterpret_cast<float *>(&rgb_);
-
-      cloud->points.push_back(pt);
-      // std::cout << "pointXYZRGB:" <<  pt << std::endl;
-    }
-
-    pcl::console::print_info("\nFound txt file.\n");
-    pcl::console::print_info("[done, ");
-    pcl::console::print_value("%g", tt.toc());
-    pcl::console::print_info(" ms : ");
-    pcl::console::print_value("%d", cloud->points.size());
-    pcl::console::print_info(" points]\n");
-
-  } else if (file_is_xyz) {
-
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-      std::cout << "Error: Could not find " << filename << std::endl;
-      return -1;
-    }
-
-    std::cout << "file opened." << std::endl;
-    double x_, y_, z_;
-
-    while (file >> x_ >> y_ >> z_) {
-
-      pcl::PointXYZRGB pt;
-      pt.x = x_;
-      pt.y = y_;
-      pt.z = z_;
-
-      cloud->points.push_back(pt);
-      // std::cout << "pointXYZRGB:" <<  pt << std::endl;
-    }
-
-    pcl::console::print_info("\nFound xyz file.\n");
-    pcl::console::print_info("[done, ");
-    pcl::console::print_value("%g", tt.toc());
-    pcl::console::print_info(" ms : ");
-    pcl::console::print_value("%d", cloud->points.size());
-    pcl::console::print_info(" points]\n");
-  }
-
-  cloud->width = (int)cloud->points.size();
-  cloud->height = 1;
-  cloud->is_dense = true;
-
-  std::string output_dir = arg_parser.get<std::string>("--output-dir");
-
+  // parse data from command line
   int surface_mode = arg_parser.get<int>("--surface");
   int normal_method = arg_parser.get<int>("--normal");
+  std::string filename = arg_parser.get<std::string>("--file");
+  std::string output_dir = arg_parser.get<std::string>("--output-dir");
 
+  // check if output dir is path
   boost::filesystem::path dirPath(output_dir);
-
-  if (not boost::filesystem::exists(dirPath) or
-      not boost::filesystem::is_directory(dirPath)) {
+  if (not boost::filesystem::exists(dirPath) or not boost::filesystem::is_directory(dirPath)) {
     pcl::console::print_error("\nError. does not exist or it's not valid: ");
     std::cout << output_dir << std::endl;
     std::exit(-1);
   }
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz(
-      new pcl::PointCloud<pcl::PointXYZ>());
+  // -----------------Read input cloud file -----------------
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+  // cloud parser object
+  CloudParserLibrary::ParserCloudFile cloud_parser;
+  cloud_parser.load_cloudfile(filename, cloud);
+
+  // set cloud metadata
+  cloud->width = (int)cloud->points.size();
+  cloud->height = 1;
+  cloud->is_dense = true;
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz(new pcl::PointCloud<pcl::PointXYZ>());
   pcl::copyPointCloud(*cloud, *cloud_xyz);
 
-  // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz_filtered (new
-  // pcl::PointCloud<pcl::PointXYZ>());
-  // cloudPointFilter(cloud_xyz,cloud_xyz_filtered);
+  // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz_filtered (new pcl::PointCloud<pcl::PointXYZ>());
+  // cloudPointFilter(cloud_xyz, cloud_xyz_filtered);
 
   pcl::PolygonMesh cloud_mesh;
   create_mesh(cloud_xyz, surface_mode, normal_method, cloud_mesh);
 
-  output_dir += "/cloud_mesh.ply";
+  // export mesh file
+  dirPath /= "cloud_mesh.ply";
+  pcl::io::savePLYFileBinary(dirPath.c_str(), cloud_mesh);
+  pcl::console::print_info("saved mesh in: %s \n", dirPath.c_str());
 
-  std::string sav = "saved mesh in:";
-  sav += output_dir;
-
-  // typedef pcl::geometry::DefaultMeshTraits <>      MeshTraits;
-  // typedef pcl::geometry::TriangleMesh <MeshTraits> Mesh;
-  // typedef pcl::geometry::MeshIO <Mesh>             MeshIO;
-
-  pcl::console::print_info(sav.c_str());
-  std::cout << std::endl;
-
-  pcl::io::savePLYFileBinary(output_dir.c_str(), cloud_mesh);
-
-  vtkObject::GlobalWarningDisplayOff(); // Disable vtk render warning
-  vizualizeMesh(cloud, cloud_mesh);
+  // -----------------Visualize mesh -----------------
+  if (arg_parser["--display"] == true) {
+    // Disable vtk render warning
+    vtkObject::GlobalWarningDisplayOff();
+    vizualizeMesh(cloud, cloud_mesh);
+  }
 
   return 0;
 }
